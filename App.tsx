522ce36ea4e253e4d27ba713d30ef20129867640
André Kovac <me@andrekovac.com>
2020-08-19 17:58:11 +0200

    Swap StyleSheet styles in App.tsx for styled-components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import styled from "styled-components/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
