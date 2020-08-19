560a97427caf6e3550382256c194154a9fb0de82
André Kovac <me@andrekovac.com>
2020-08-19 18:50:46 +0200

    Implement ColoredButtonsScreen  inside of App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
+import React from "react";
+
+type Props = {
+  backgroundColor?: string,
+  title?: string,
+}
+const Box = ({ backgroundColor = "red", title = "No title" }: Props) => (
+  <Wrapper backgroundColor={backgroundColor}>
+    <Title>{title}</Title>
+  </Wrapper>
+);
 
 export default function App() {
   return (
     <Container>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Box backgroundColor={'#ee1d1d'} title={'one'} />
+      <Box backgroundColor={'#309930'} title={'two'} />
+      <Box backgroundColor={'#3d3dcc'} title={'three'} />
       <StatusBar style="auto" />
     </Container>
   );
@@ -14,7 +25,24 @@ export default function App() {
 
 const Container = styled.View`
   flex: 1;
-  background-color: #fff;
-  align-items: center;
   justify-content: center;
+`;
+
+type WrapperProps = { backgroundColor?: string };
+const Wrapper = styled.View<WrapperProps>`
+  background-color: ${(props) => props.backgroundColor || "blue"};
+  border-radius: 5px;
+
+  margin: 10px;
+
+
+  flex: 0.25;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 24px;
 `;
