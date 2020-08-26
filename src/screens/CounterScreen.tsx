5716714e07ec5a0c7d219e6c0f63ada70279f4fa
André Kovac <me@andrekovac.com>
2020-08-26 16:07:23 +0200

    Add native-base and use it with counter

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,6 +1,23 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
+import React, { useState, FunctionComponent } from "react";
 import styled from "styled-components/native";
+import { StyleProvider, Badge, Button, Text } from "native-base";
+import { Row, Grid } from "react-native-easy-grid";
+
+// @ts-ignore
+import getTheme from "../native-base-theme/components";
+// @ts-ignore
+import material from "../native-base-theme/variables/material";
+
+const CounterButton: FunctionComponent<{ onPress: () => void }> = ({
+  onPress,
+  children,
+}) => (
+  <CenteredHorizontally>
+    <Button rounded onPress={onPress}>
+      <Text>{children}</Text>
+    </Button>
+  </CenteredHorizontally>
+);
 
 const CounterScreen = () => {
   const [count, setCount] = useState<number>(0);
@@ -8,31 +25,41 @@ const CounterScreen = () => {
   const increment = () => setCount((prev: number) => prev + 1);
 
   return (
-    <Container>
-      <ClickedText>Clicked {count} times</ClickedText>
-      <Button onPress={increment}>
-        <Text>Increment</Text>
-      </Button>
-    </Container>
+    <StyleProvider style={getTheme(material)}>
+      <Grid>
+        <Row size={25}>
+          <Wrapper row>
+            <Text>{`Clicked `}</Text>
+            <CenteredHorizontally>
+              <Badge primary>
+                <Text>{count}</Text>
+              </Badge>
+            </CenteredHorizontally>
+            <Text>{` times`}</Text>
+          </Wrapper>
+        </Row>
+        <Row size={75}>
+          <Wrapper>
+            <CounterButton onPress={increment}>Increment</CounterButton>
+          </Wrapper>
+        </Row>
+      </Grid>
+    </StyleProvider>
   );
 };
 
-const Container = styled.View`
+/**
+ * This Wrapper around `Button` and `Badge` is necessary to align it horizontally
+ */
+const CenteredHorizontally = styled.View`
+  align-items: center;
+`;
+
+const Wrapper = styled.View<{ row?: boolean }>`
   flex: 1;
+  flex-direction: ${(props) => (props.row ? "row" : "column")};
   align-items: center;
   justify-content: center;
-`;
-
-const Button = styled.TouchableOpacity`
-  background-color: #74e685;
-  border-radius: 10px;
-  padding: 10px;
-  margin: 10px;
-`;
-
-const ClickedText = styled.Text`
-  font-size: 20px;
-  margin-bottom: 20px;
 `;
 
 export default CounterScreen;
