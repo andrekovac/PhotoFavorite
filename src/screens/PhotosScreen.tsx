ab60427e946a5ec45767ae19caa71e71cf3e9b7c
André Kovac <me@andrekovac.com>
2020-09-09 18:05:54 +0200

    Show Alert on Press

--- src/screens/PhotosScreen.tsx
+++ src/screens/PhotosScreen.tsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, FC } from "react";
 import styled from "styled-components/native";
-import { ActivityIndicator, FlatList } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 type ItemT = {
   id: string;
@@ -8,8 +13,14 @@ type ItemT = {
   download_url: string;
 };
 
-const Item = ({ download_url }: Pick<ItemT, "download_url">) => (
-  <ItemImage source={{ uri: download_url }} />
+const Item = ({ download_url, author }: Omit<ItemT, "id">) => (
+  <TouchableOpacity
+    onPress={() => {
+      Alert.alert("Artist", author, [{ text: "OK" }], { cancelable: false });
+    }}
+  >
+    <ItemImage source={{ uri: download_url }} />
+  </TouchableOpacity>
 );
 
 const PhotosScreen = () => {
@@ -26,7 +37,7 @@ const PhotosScreen = () => {
     } catch (error) {
       console.error(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
 
@@ -35,7 +46,7 @@ const PhotosScreen = () => {
   }, []);
 
   const renderItem = ({ item }: { item: ItemT }) => (
-    <Item download_url={item.download_url} />
+    <Item download_url={item.download_url} author={item.author} />
   );
 
   return isLoading ? (
