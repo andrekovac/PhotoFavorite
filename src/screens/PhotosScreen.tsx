ca566da8daf67ed2d8157ad6575fc2e6ecfe9eb9
André Kovac <me@andrekovac.com>
2020-09-09 09:48:32 +0200

    Create list component with dummy list

--- src/screens/PhotosScreen.tsx
+++ src/screens/PhotosScreen.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
+
+type ItemT = {
+  id: string;
+  title: string;
+};
+
+const Item = ({ title }: { title: string }) => (
+  <ItemWrapper>
+    <ItemText>{title}</ItemText>
+  </ItemWrapper>
+);
 
 const PhotosScreen = () => {
+  const placeholderList: ItemT[] = Array(100)
+    .fill(0)
+    .map((_, i) => ({ id: String(i + 1), title: `Title ${i + 1}` }));
+
+  const renderItem = ({ item }: { item: ItemT }) => <Item title={item.title} />;
+
   return (
     <Wrapper>
-      <Text>Photos Screen</Text>
+      <FlatList
+        data={placeholderList}
+        renderItem={renderItem}
+        keyExtractor={(item: ItemT) => item.id}
+      />
     </Wrapper>
   );
 };
@@ -16,6 +37,22 @@ const Wrapper = styled.View`
   /* children */
   justify-content: center;
   align-items: center;
+`;
+
+const ItemWrapper = styled.View`
+  height: 300px;
+  width: 300px;
+  background-color: #3f87f5;
+  margin: 5px 0;
+
+  /* children */
+  justify-content: center;
+  align-items: center;
+`;
+
+const ItemText = styled.Text`
+  color: white;
+  font-size: 22px;
 `;
 
 export default PhotosScreen;
