ca5b10806b51cc6b0a026352c848a7753f3bd5b1
André Kovac <me@andrekovac.com>
2020-10-22 00:01:44 +0200

    Improve coupling of PhotosScreen and PhotoList: Don't pass `navigation` prop down to PhotoList component from Screen.

--- src/components/PhotoList.tsx
+++ src/components/PhotoList.tsx
@@ -1,36 +1,27 @@
 import React, { FunctionComponent } from "react";
 import { ActivityIndicator, FlatList } from "react-native";
-import { NavigationProp } from "@react-navigation/native";
 
 import Item, { ItemT } from "./Item";
 import { PhotosDataT } from "../store/slices/photos";
-import { RootStackParamList } from "../types";
 
 interface PhotoListPropsT {
   data: PhotosDataT;
   isLoading?: boolean;
-  navigation?: NavigationProp<RootStackParamList>;
+  onItemPress?: (item: ItemT) => void;
 }
 
 const PhotoList: FunctionComponent<PhotoListPropsT> = ({
   data,
   isLoading = false,
-  navigation,
+  onItemPress,
 }) => {
-  const handlePress = (item: ItemT) => {
-    // Use of non-null assertion operator
-    navigation!.navigate("Modal", {
-      item,
-    });
-  };
-
   const renderItem: FunctionComponent<{ item: ItemT }> = ({ item }) => (
     <Item
       id={item.id}
       download_url={item.download_url}
       author={item.author}
       isFavorite={item.isFavorite}
-      onPress={navigation && (() => handlePress(item))}
+      onPress={onItemPress && (() => onItemPress(item))}
     />
   );
 
--- src/screens/PhotosScreen.tsx
+++ src/screens/PhotosScreen.tsx
@@ -3,18 +3,25 @@ import styled from "styled-components/native";
 
 import usePhotos from "../hooks/redux/usePhotos";
 import PhotoList from "../components/PhotoList";
+import { ItemT } from "../components/Item";
 
 import { ModalScreenProps } from "../types";
 
 const PhotosScreen = ({ navigation }: ModalScreenProps) => {
   const { data, isLoading } = usePhotos();
 
+  const handleItemPress = (item: ItemT) => {
+    navigation.navigate("Modal", {
+      item,
+    });
+  };
+
   return (
     <ListWrapper>
       <PhotoList
         data={data}
         isLoading={isLoading}
-        navigation={navigation}
+        onItemPress={handleItemPress}
       />
     </ListWrapper>
   );
