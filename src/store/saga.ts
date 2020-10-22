8d2e9ed11c146b60f5e8b257a01acc0a300df23d
André Kovac <me@andrekovac.com>
2020-10-22 22:08:29 +0200

    Implement useless saga about numbers which demonstrates some of the capabilities of sagas

--- src/store/saga.ts
+++ src/store/saga.ts
@@ -1,10 +1,21 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+import {
+  all,
+  call,
+  delay,
+  fork,
+  put,
+  race,
+  select,
+  take,
+  takeLatest,
+} from "redux-saga/effects";
 
 import {
   fetchPhotosError,
   fetchPhotosStart,
   fetchPhotosSuccess,
 } from "../store/slices/photos";
+import { increment, decrement, reset } from "../store/slices/counter";
 
 import { fetchPhotos } from "../api/photos";
 
@@ -22,4 +33,36 @@ function* fetchSaga() {
   yield takeLatest(fetchPhotosStart.type, fetchData);
 }
 
-export default fetchSaga;
+function* countTill20() {
+  let count = yield select((state) => state.count);
+
+  while (count < 20) {
+    yield put(increment());
+    count = count + 1;
+    yield delay(1000);
+  }
+}
+
+function* numbersSaga() {
+  // instead of while(true), takeEvery or takeLatest can be used
+  while (true) {
+    yield take(reset.type);
+
+    const { cancel } = yield race({
+      task: call(countTill20),
+      cancel: take(decrement.type),
+    });
+
+    if (cancel) {
+      yield call(console.log, "Automatic count cancelled!");
+    }
+  }
+}
+
+function* rootSaga() {
+  yield all([
+    fork(fetchSaga),
+    fork(numbersSaga),
+  ]);
+}
+export default rootSaga;
