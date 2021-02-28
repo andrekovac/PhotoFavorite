f85c23d5ac11827a6effc43a242a42038442f6f4
André Kovac <me@andrekovac.com>
2021-02-28 11:15:00 +0100

    Make width and height dynamic depending on screen size with useLayout hook

--- src/screens/PhotosScreen.tsx
+++ src/screens/PhotosScreen.tsx
@@ -1,31 +1,42 @@
 import React from "react";
 import styled from "styled-components/native";
-import { FlatList } from "react-native";
+import { FlatList, ListRenderItem } from "react-native";
+import { useLayout } from "@react-native-community/hooks";
 
 type ItemT = {
   id: string;
   title: string;
 };
 
-const Item = ({ title }: { title: string }) => (
-  <ItemWrapper>
-    <ItemText>{title}</ItemText>
-  </ItemWrapper>
-);
+const Item = ({ title }: { title: string }) => {
+  const { onLayout, ...layout } = useLayout();
+
+  // Log Layout values
+  // console.log("layout: ", layout);
+
+  return (
+    <ItemWrapper onLayout={onLayout} style={{ height: layout.width }}>
+      <ItemText>{title}</ItemText>
+    </ItemWrapper>
+  );
+};
 
 const PhotosScreen = () => {
   const placeholderList: ItemT[] = Array(100)
     .fill(0)
     .map((_, i) => ({ id: String(i + 1), title: `Title ${i + 1}` }));
 
-  const renderItem = ({ item }: { item: ItemT }) => <Item title={item.title} />;
+  const renderItem: ListRenderItem<ItemT> = ({ item }) => (
+    <Item title={item.title} />
+  );
 
   return (
     <Wrapper>
       <FlatList
+        style={{ width: "100%" }}
         data={placeholderList}
         renderItem={renderItem}
-        keyExtractor={(item: ItemT) => item.id}
+        keyExtractor={(item) => item.id}
       />
     </Wrapper>
   );
@@ -33,6 +44,7 @@ const PhotosScreen = () => {
 
 const Wrapper = styled.View`
   flex: 1;
+  padding: 0 20px;
 
   /* children */
   justify-content: center;
@@ -40,8 +52,6 @@ const Wrapper = styled.View`
 `;
 
 const ItemWrapper = styled.View`
-  height: 300px;
-  width: 300px;
   background-color: #3f87f5;
   margin: 5px 0;
 
