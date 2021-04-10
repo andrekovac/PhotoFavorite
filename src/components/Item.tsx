e4e56b9f3c61a384fbe73e516ef9ba6ef65a9191
Christian Ost <hallo@christianost.de>
2021-04-10 15:51:46 +0200

    fix: proper formatting, better threshold

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback } from "react";
-import { Alert } from "react-native";
-import styled from "styled-components/native";
+import React from 'react';
+import { Alert } from 'react-native';
+import styled from 'styled-components/native';
 
-import useFavorites from "../hooks/redux/useFavorites";
-import Icon from "./Icon";
-import SwipeableElement from "./SwipeableElement";
+import useFavorites from '../hooks/redux/useFavorites';
+import Icon from './Icon';
+import SwipeableElement from './SwipeableElement';
 
 const SIZE = 300;
 
@@ -20,25 +20,25 @@ export type ItemT = {
  */
 const Item = ({ id, author, download_url, isFavorite }: ItemT) => {
   const [_, updateFavorite] = useFavorites();
-  const handleSwipeEnd = (direction: "left" | "right") => {
-    updateFavorite(id, direction === "right");
+  const handleSwipeEnd = (direction: 'left' | 'right') => {
+    updateFavorite(id, direction === 'right');
   };
 
   return (
     <SwipeableElement
-      threshold={SIZE / 2}
+      threshold={SIZE / 3}
       onSwipeEndOverThreshold={handleSwipeEnd}
     >
       <Wrapper
         onPress={() => {
-          Alert.alert("Photographer", author, [{ text: "OK" }], {
+          Alert.alert('Photographer', author, [{ text: 'OK' }], {
             cancelable: false,
           });
         }}
       >
         <Image source={{ uri: download_url }} />
         <FavoriteButton onPress={() => updateFavorite(id, !isFavorite)}>
-          <Icon name={isFavorite ? "md-star" : "md-star-outline"} />
+          <Icon name={isFavorite ? 'md-star' : 'md-star-outline'} />
         </FavoriteButton>
       </Wrapper>
     </SwipeableElement>
